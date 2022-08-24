59cd8a3d033aa67af617904e11b0c03bd4a3e93a
yury.musienko <yury.musienko@helmes.com>
2022-08-24 08:35:50 +0300

    Third folder - basics of react

--- src/app/App.js
+++ src/app/App.js
@@ -3,23 +3,68 @@ import Tutor from "../entity/Tutor";
 
 class App extends Component {
     state = {
-        tutors: [{ name: 'Alina Musienko', phoneNumber: '80297263627' }, { name: 'Yury Musienko', phoneNumber: '80298344491' }, { name: 'Alex Hunter', phoneNumber: '80291111111' }],
-        title: 'Tutors'
+        tutors: [{ name: 'Alina Musienko', phoneNumber: '80297263627', qualification: 'Speech therapist' }, { name: 'Yury Musienko', phoneNumber: '80298344491', qualification: 'Software developer' }, { name: 'Alex Hunter', phoneNumber: '80291111111', qualification: 'Math teacher' }],
+        title: 'Tutors',
+        showTutors: false
     }
 
-    handleChangeTitle = () => {
-        console.log('hello');
-    };
+    onChangeName(name, index) {
+        const tutor = this.state.tutors[index];
+        tutor.name = name;
+        const tutors = [...this.state.tutors];
+        tutors[index] = tutor;
+        this.setState({ tutors });
+    }
+
+    handleInput = (event) => {
+        this.setState({
+            title: event.target.value
+        });
+    }
+
+    handleDelete(index) {
+        const tutors = [...this.state.tutors];
+        tutors.splice(index, 1);
+        this.setState({tutors});
+    }
+
+    toggleTutorsHandler = () => {
+        this.setState({
+            showTutors: !this.state.showTutors
+        });
+    }
 
     render() {
         const tutors = this.state.tutors;
         return (
-            <div style={{ textAlign: "center", color: "blue", fontSize: "30px" }}>
-                <h1>{this.state.title}</h1>
-                <button onClick={this.handleChangeTitle}>Change title</button>
-                <Tutor name={tutors[0].name} phoneNumber={tutors[0].phoneNumber} />
-                <Tutor name={tutors[1].name} phoneNumber={tutors[1].phoneNumber} />
-                <Tutor name={tutors[2].name} phoneNumber={tutors[2].phoneNumber} />
+            <div style={{
+                textAlign: "center",
+                color: "black",
+                fontSize: "15px",
+            }}>
+                <div>
+                    <h1>{this.state.title}</h1>
+                    <input
+                        type='text'
+                        placeholder='type new title'
+                        onChange={this.handleInput}
+                    />
+                    <button
+                        onClick={this.toggleTutorsHandler}
+                    >Toggle Tutors</button>
+                </div>
+                {this.state.showTutors ? this.state.tutors.map((tutor, index) => {
+                    return (
+                        <Tutor
+                            key={index}
+                            name={tutor.name}
+                            qualification={tutor.qualification}
+                            phoneNumber={tutor.phoneNumber}
+                            onDelete={this.handleDelete.bind(this, index)}
+                            onChangeName={event => this.onChangeName(event.target.value, index)}
+                        />
+                    )
+                }) : null}
             </div>
         );
 
--- src/entity/Tutor.js
+++ src/entity/Tutor.js
@@ -2,9 +2,24 @@ import React from "react";
 
 function Tutor(props) {
     return (
-        <div>
-            <h1>Name: {props.name}</h1>
+        <div style={{
+            border: '1px solid #ccc',
+            margin: '10px',
+            padding: '10px',
+            display: 'inline-block',
+            marginLeft: 'auto',
+            marginRight: 'auto'
+        }}>
+            <p>Name: <strong>{props.name}</strong></p>
+            <p>Qualification: <strong>{props.qualification}</strong></p>
             <p>Phone Number: <strong>{props.phoneNumber}</strong></p>
+            <input
+                onChange={props.onChangeName}
+                value={props.name}
+            />
+            <button
+                onClick={props.onDelete}
+            >Delete</button>
         </div>
     );
 }
